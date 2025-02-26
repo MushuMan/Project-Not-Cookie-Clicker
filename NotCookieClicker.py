5319c305a8bf6ec56cd4f8bc5d2051e0a22ebb09
rubbor-duck <nelson.jordan10@hotmail.com>
2025-02-26 12:34:12 -0800

    Working on base functions
    Added Fonts and Coockie Count Function

--- NotCookieClicker.py
+++ NotCookieClicker.py
@@ -2,13 +2,30 @@ import pygame
 import time 
 import random
 
-# Test comment
-
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Not Cookie Clicker")
 
+# Font
+# Font path needs to be defined
+font_path = 
+font = pygame.font.Font(None, 36)
+
 BG = pygame.transform.scale(pygame.image.load("spacebg.jpg"), (WIDTH, HEIGHT))
+
+# Intalizing Cookie Count
+cookie_count = 0
+
+# Function to handle cookie clicks
+def click_cookie():
+    global cookie_count
+    cookie_count =+ 1
+
+
+# Draw the Cookie Count
+cookie_label = font.render(f"Cookies: {int(cookie_count)}", True, BLACK)
+WIN.blit(cookie_label, (20,20))
+
 
 def draw():
     WIN.blit(BG, (0, 0))
