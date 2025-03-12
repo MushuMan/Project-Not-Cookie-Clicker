07ea21d475abf7911b5ae459a294ff51c701b236
rubbor-duck <nelson.jordan10@hotmail.com>
2025-03-12 12:29:27 -0700

    Resizable Screen Time Boiz
    I added a function that allows the screen to resize and centers the images/buttons to the new window size.

--- NotCookieClicker.py
+++ NotCookieClicker.py
@@ -3,24 +3,31 @@ import time
 import random
 
 WIDTH, HEIGHT = 1000, 800
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Not Cookie Clicker")
+WIN = pygame.display.set_mode((WIDTH, HEIGHT),pygame.RESIZABLE)
+pygame.display.set_caption("Pringle Clicker")
 
 # Font
 pygame.font.init()
 font = pygame.font.Font("times new roman.ttf", 36)
 
-BG = pygame.transform.scale(pygame.image.load("spacebg.jpg"), (WIDTH, HEIGHT))
-
-# Initializing Cookie Count
+# Initializing Pringle Count
 pringle_count = 0
 text_color = (255, 255, 255)
 
+#Background Image
+BG = pygame.transform.scale(pygame.image.load("spacebg.jpg"), (WIDTH, HEIGHT))
+
+#Centers Images and Buttons when resizing window
+def setGet(screenSize):
+    global WIDTH, HEIGHT, BG
+    WIDTH, HEIGHT = screenSize
+    BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
+
 def draw():
     WIN.blit(BG, (0, 0))
-    # Draw the Cookie Count
-    cookie_label = font.render(f"Pringles: {int(pringle_count)}", True, text_color)
-    WIN.blit(cookie_label, (20, 20))
+    # Draw the Pringle Count
+    pringle_label = font.render(f"Pringles: {int(pringle_count)}", True, text_color)
+    WIN.blit(pringle_label, (20, 20))
 
 # Setting Up Main Button for Pringles
 class Button:
@@ -38,6 +45,12 @@ class Button:
         self.font = pygame.font.Font("times new roman.ttf", 36)
         self.original_image = pygame.image.load(image).convert_alpha()
         self.image = pygame.transform.scale(self.original_image, (self.rect.width, self.rect.height))
+        self.update_position(x,y)
+
+    def update_position(self, x, y):
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(x - (self.width // 2), y - (self.height // 2), self.width, self.height)
 
     def draw(self, surface):
         # Change Color on Hover
@@ -62,6 +75,7 @@ class Button:
         return mouse_click[0] and self.rect.collidepoint(pygame.mouse.get_pos())
     
     def change_size(self, width, height):
+        self.update_position(self.x, self.y)
         self.rect = pygame.Rect((self.x - (width // 2)), (self.y - (height // 2)), width, height)
         self.image = pygame.transform.scale(self.original_image, (self.rect.width, self.rect.height))
 
@@ -81,6 +95,10 @@ def main():
                 if button.rect.collidepoint(event.pos):  # Check if the button was clicked
                     global pringle_count  # Use global to modify the pringle_count variable
                     pringle_count += 1
+
+            if event.type == pygame.VIDEORESIZE:
+                setGet(event.size)
+                button.update_position(WIDTH // 2, HEIGHT //2)
         
         draw()
         button.draw(WIN)  # Draw the button on the main window
